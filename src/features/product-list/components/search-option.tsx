29f8764a54dafe25bbc33bb142c7239c77e6ac40
ian <im-ian@naver.com>
2025-06-20 17:25:42 +0900

    fix: 불필요한 import 제거

--- src/features/product-list/components/search-option.tsx
+++ src/features/product-list/components/search-option.tsx
@@ -11,7 +11,6 @@ import {
   FormItem,
   FormLabel,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Switch } from "@/components/ui/switch";
 import {
   Popover,
