bdda1e7e0d1fdb904751710f0a830e1b9fe63d02
ian <im-ian@naver.com>
2025-06-20 18:27:34 +0900

    chore: 불필요한 파일 제거

--- src/features/shared/components/range-slider.tsx
+++
@@ -1,9 +0,0 @@
-import RangeSliderInput, {
-  type ReactRangeSliderInputProps,
-} from "react-range-slider-input";
-
-import "react-range-slider-input/dist/style.css";
-
-export function RangeSlider(props: ReactRangeSliderInputProps) {
-  return <RangeSliderInput {...props} />;
-}
