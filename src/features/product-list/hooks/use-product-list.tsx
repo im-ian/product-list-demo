4a468723c9a95954268210a615ae4286374ce7f7
ian <im-ian@naver.com>
2025-06-20 11:29:23 +0900

    feat: product-list 훅 페이지네이션 추가

--- src/features/product-list/hooks/use-product-list.tsx
+++ src/features/product-list/hooks/use-product-list.tsx
@@ -1,19 +1,34 @@
 import { useQuery, queryOptions } from "@tanstack/react-query";
 import type { ProductListResponse } from "@/features/product-list/type/product-list";
 
-const productListQueryOptions = queryOptions<ProductListResponse>({
-  queryKey: ["product-list"] as const,
-  queryFn: async () => {
-    const response = await fetch("/api/product-list");
-    if (!response.ok) {
-      throw new Error("Failed to fetch product list.");
-    }
-    return response.json();
-  },
-  staleTime: 5 * 60 * 1000,
-  refetchOnWindowFocus: false,
-});
+interface UseProductListParams {
+  page: number;
+  pageSize: number;
+}
 
-export function useProductList() {
-  return useQuery(productListQueryOptions);
+const createProductListQueryOptions = ({
+  page = 1,
+  pageSize = 10,
+}: UseProductListParams) =>
+  queryOptions<ProductListResponse>({
+    queryKey: ["product-list", page, pageSize] as const,
+    queryFn: async () => {
+      const params = new URLSearchParams({
+        page: page.toString(),
+        pageSize: pageSize.toString(),
+      });
+
+      const response = await fetch(`/api/product-list?${params}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch product list.");
+      }
+      return response.json();
+    },
+    staleTime: 5 * 60 * 1000,
+    refetchOnWindowFocus: false,
+  });
+
+export function useProductList(params: UseProductListParams) {
+  const queryOptions = createProductListQueryOptions(params);
+  return useQuery(queryOptions);
 }
